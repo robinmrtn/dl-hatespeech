340c741ba5b532615a50176949f2fab1fc867b61
martrob <45385434+martrob@users.noreply.github.com>
2019-11-11 22:31:42 +0100

    add history to Trainer

--- models/Training/trainer.py
+++ models/Training/trainer.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
+import time
+import pandas as pd
 
 class Trainer:
   def __init__(self, model, optimizer, seed=9):
@@ -9,13 +11,14 @@ class Trainer:
     self.seed = seed
 
     self.loss_fn = tf.keras.losses.BinaryCrossentropy()
-
+    
+    self.train_acc = tf.keras.metrics.BinaryAccuracy()
     self.train_acc = tf.keras.metrics.BinaryAccuracy()
     self.val_acc = tf.keras.metrics.BinaryAccuracy()
         
     self.val_precision = tf.keras.metrics.Precision()
     self.val_recall = tf.keras.metrics.Recall()
-
+    
 
   def train(self,
             dataset,
@@ -24,40 +27,60 @@ class Trainer:
     X_train, X_val, y_train, y_val = train_test_split(dataset[0],
                                                       dataset[1], 
                                                         test_size=0.1, 
-                                                        random_state=9)
+                                                        random_state=SEED)
+    
+
     train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))    
-    train_dataset = train_dataset.shuffle(buffer_size=1024,seed=self.seed).batch(batch_size)
+    train_dataset = train_dataset.shuffle(buffer_size=1024,seed=SEED).batch(batch_size)
 
     val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val))
     val_dataset = val_dataset.batch(32)
-
+    history_columns = ["train_loss","train_acc","val_loss",
+                       "val_acc","val_precision","val_recall",
+                       "val_f1","runtime"]
+    history_array = np.zeros((epochs,len(history_columns)))
     
-
     for epoch in range(epochs):
       epoch +=1
       print(f'Start of epoch {epoch}:')
-      
+      start_time = time.time()
       for step, (X_batch_train, y_batch_train) in enumerate(train_dataset):
         loss_value = self.train_batch(X_batch_train, y_batch_train)
        
       print(f'Train Loss on epoch {epoch}: {loss_value}')
       print(f'Train Accuracy on epoch {epoch}: {self.train_acc.result()}')
 
-      self.train_acc.reset_states()
+      
 
       for X_batch_val, y_batch_val in val_dataset:
        
         loss_value_val = self.validate_batch(X_batch_val, y_batch_val)
       
-      print(f"Val Loss on epoch {epoch}: {loss_value_val}")
-      print(f"Val Accuracy on epoch {epoch}: {self.val_acc.result()}")
-      print(f"Val Precision on epoch {epoch}: {self.val_precision.result()}")
-      print(f"Val Recall on epoch {epoch}: {self.val_recall.result()}")
+      val_f1 = 2 * (self.val_recall.result() * self.val_precision.result()) / (self.val_recall.result() + self.val_precision.result())
+      end_time = time.time()
+      runtime = end_time-start_time
+      history_array[epoch-1] = np.array([loss_value,
+                                         self.train_acc.result(),
+                                         loss_value_val,
+                                         self.val_acc.result(),
+                                         self.val_precision.result(),
+                                         self.val_recall.result(),
+                                         val_f1,
+                                         runtime])
+      print(f"Val Loss: {loss_value_val}")
+      print(f"Val Accuracy: {self.val_acc.result()}")
+      print(f"Val Precision: {self.val_precision.result()}")
+      print(f"Val Recall: {self.val_recall.result()}")
+      
+      self.train_acc.reset_states()
       self.val_acc.reset_states()
       self.val_precision.reset_states()
       self.val_recall.reset_states()
       
-  
+    history_df = pd.DataFrame(data=history_array, columns=history_columns)
+
+    return history_df
+
   @tf.function
   def train_batch(self, X, y):
     with tf.GradientTape() as tape:
@@ -78,11 +101,16 @@ class Trainer:
   def validate_batch(self, X, y):
 
     val_logits = self.model(X)
-    val_logits = tf.reshape(val_logits,shape=y.shape)
+    #val_logits = tf.reshape(val_logits,shape=y.shape)
+    y = tf.reshape(y ,shape=val_logits.shape)
     self.val_acc(y, val_logits)
+    #print(f"logits ndim: {val_logits.ndim} shape: {val_logits.shape}")
+    #print(f"y ndim: {y.ndim} shape: {y.shape}")
+    #self.val_f1(y, val_logits)
     self.val_precision(y, val_logits)
     self.val_recall(y, val_logits)
 
     loss_value = self.loss_fn(y, val_logits)
 
     return loss_value
+
